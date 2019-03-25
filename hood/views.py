0c24d2134f8a7f77081b01e4d6b7d32e6041eada
Chiuri254 <joseshiory@gmail.com>
2019-03-25 09:24:41 +0300

    Add category model

--- hood/views.py
+++ hood/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect
+from .models import Image,Category,Location
+from django.http import Http404
 
 # Create your views here.
 def welcome(request):
-    return render(request,"welcome.html")
+    images = Image.objects.all()
+    locations = Location.objects.all()
+    return render(request,'welcome.html',{'images':images,'locations':locations})
 
+def display_location(request,location_id):
+    try:
+        locations = Location.objects.all()
+        location = Location.objects.get(id = location_id)
+        images = Image.objects.filter(image_location = location.id)
+    except:
+        raise Http404()
+    return render(request,'location.html',{'location':location,'images':images,'locations':locations})
 
-    
+def search_category(request):
+    locations = Location.objects.all()
+    if 'category' in request.GET and request.GET['category']:
+        search_term = (request.GET.get('category')).title()
+        searched_images = Image.search_by_category(search_term)
+        message = f'{search_term}'
+        return render(request,'search.html',{'message':message,'images':searched_images,'locations':locations})
+
+    else:
+        message = "You haven't searched for any category"
+        return render(request,'search.html',{'message':message,'locations':locations})
